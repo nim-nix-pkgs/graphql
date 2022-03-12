4090ce564ec2160f7512cf4d02146821a6ac529e
hugosenari <hugosenari@gmail.com>
2022-03-12 03:17:11 -0300

    chore: re index graphql

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''GraphQL parser, server and client implementation'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."graphql-master".url = "path:./master";
+  inputs."graphql-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."graphql-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
